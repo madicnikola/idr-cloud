e5a2e868d757c4025979138ee0c52bb9ebfba12c
madicnikola <madinikola@outlook.com>
2023-07-12 22:21:14 +0200

    postgre

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+import psycopg2
 from flask import Flask
 from flask_jwt_extended import JWTManager
-from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+load_dotenv()
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['JWT_SECRET_KEY'] = 'tajni-kljuc'  # Ovo treba da bude tajni ključ. Ne koristite ovu vrednost u produkciji.
+    app.config['JWT_SECRET_KEY'] = f"{os.getenv('JWT_SECRET')}"  # Ovo treba da bude tajni ključ. Ne koristite ovu vrednost u produkciji.
     jwt = JWTManager(app)
     # configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    connection_string = f"dbname='{os.getenv('DB_NAME')}' user='{os.getenv('DB_USERNAME')}' host='{os.getenv('DB_HOST')}' password='{os.getenv('DB_PASSWORD')}' port='{os.getenv('DB_PORT')}'"
 
-    db.init_app(app)
+    # Establish connection to PostgresSQL
+    conn = psycopg2.connect(connection_string)
+    cursor = conn.cursor()
 
     # import models here
     from .models import user, product, category, order
