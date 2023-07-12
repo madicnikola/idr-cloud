681638ef1035925c9fc8d113da9395124d105ffd
madicnikola <madicnikola@outlook.com>
2023-07-13 00:21:49 +0200

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,27 @@
-import os
-from dotenv import load_dotenv
-
-import psycopg2
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv()
-
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['JWT_SECRET_KEY'] = f"{os.getenv('JWT_SECRET')}"  # Ovo treba da bude tajni ključ. Ne koristite ovu vrednost u produkciji.
+    app.config['JWT_SECRET_KEY'] = 'secret-key'  # Replace with your secret key. Do not use this value in production.
     jwt = JWTManager(app)
-    # configuration
-    connection_string = f"dbname='{os.getenv('DB_NAME')}' user='{os.getenv('DB_USERNAME')}' host='{os.getenv('DB_HOST')}' password='{os.getenv('DB_PASSWORD')}' port='{os.getenv('DB_PORT')}'"
 
-    # Establish connection to PostgresSQL
-    conn = psycopg2.connect(connection_string)
-    cursor = conn.cursor()
+    # Configuration for PostgreSQL
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db:5432/postgres'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # import models here
+    db.init_app(app)
+
+    # Import models here
     from .models import user, product, category, order
 
     with app.app_context():
-        db.create_all()  # create tables
+        db.create_all()  # Create tables
 
-    # import routes here
+    # Import routes here
     from .routes import setup_routes
     setup_routes(app)
 
